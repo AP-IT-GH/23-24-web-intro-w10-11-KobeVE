c93872ee10316d03bb89b628d7e2e99c8e165fab
KobeVE <kobe.vaneyck@student.ap.be>
2024-05-07 11:32:23 +0200

    7/05

--- js/mapScript.js
+++ js/mapScript.js
@@ -3,6 +3,13 @@ Gebruik hiervoor de documentatie op https://leafletjs.com/
 */
 
 let map = L.map('apMap').setView([51.23009, 4.41616], 17);
+let coffee = L.icon({
+    iconUrl : "./images/coffee.png",
+    
+    iconSize:     [38, 95], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -13,7 +20,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // bepaal de rechthoek rondom het gebouw van AP
 let bounds = [[51.23041, 4.4155], [51.22991, 4.41675]];
 // kleur de rechthoek in met de rode AP-kleur
-L.rectangle(bounds, {color: "#e60005", weight: 1}).addTo(map);
+L.coffee(bounds, {color: "#e60005", weight: 1}).addTo(map);
 // plaats een marker met als tekst "AP-Hogeschool" en eronder "Ellermanstraat 33"
-let apMarker = L.marker([51.23009, 4.41616]).addTo(map);
+L.marker([51.23009, 4.41616]), {icon:coffee}.addTo(map);
 apMarker.bindPopup("<b>AP-Hogeschool</b><br>Ellermanstraat 33").openPopup();
